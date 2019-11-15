fbc2cd71577d464e086ea5d925a40e9fe0bd3741
mr X <53136476+hmei222@users.noreply.github.com>
2019-11-15 15:16:36 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Call
-Spam Call Termux
+# admin_finder cli based
+# Information List : 4001 list admin
+# contact : mrcakil@programmer.net
+# Greetz : IndoXploit - AnonCyberTeam - 99Syndicate
+
+# how to install :
+# git clone https://github.com/mrcakil/admin_finder
+# cd admin_finder
+# chmod +x adfin.php cakil_wordlist.txt
+# php adfin.php
---
+++ ilhamgans.php
@@ -0,0 +1,53 @@
+<?php
+// Author Name : Mr.cakil
+// Information List : 4001 list
+// contact here : mrcakil@programmer.net
+// Fb : http://facebook.com/dendisaimam.dendisaimam.1
+// greetz : IndoXploit - AnonCyberTeam - 99syndicate
+
+print "   
+	
+ █████╗ ██████╗ ███╗   ███╗██╗███╗   ██╗    ███████╗██╗███╗   ██╗██████╗ ███████╗██████╗ 
+██╔══██╗██╔══██╗████╗ ████║██║████╗  ██║    ██╔════╝██║████╗  ██║██╔══██╗██╔════╝██╔══██╗
+███████║██║  ██║██╔████╔██║██║██╔██╗ ██║    █████╗  ██║██╔██╗ ██║██║  ██║█████╗  ██████╔╝
+██╔══██║██║  ██║██║╚██╔╝██║██║██║╚██╗██║    ██╔══╝  ██║██║╚██╗██║██║  ██║██╔══╝  ██╔══██╗
+██║  ██║██████╔╝██║ ╚═╝ ██║██║██║ ╚████║    ██║     ██║██║ ╚████║██████╔╝███████╗██║  ██║
+╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝    ╚═╝     ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+                                                                                         
+                Admin Finder - coded by ./Mr.cakil
+  Information list : 4001 list
+  Thanks to  :IndoXploit - AnonCyberTeam - 99syndicate
+";
+
+echo "masukan site  : ";
+$target = trim(fgets(STDIN));
+$list = "cakil_wordlist.txt";
+if(!preg_match("/^http:\/\//",$target) AND !preg_match("/^https:\/\//",$target)){
+	$targetnya = "http://$target";
+}else{
+	$targetnya = $target;
+}
+
+$buka = fopen("$list","r");
+$ukuran = filesize("$list");
+$baca = fread($buka,$ukuran);
+$lists = explode("\r\n",$baca);
+
+foreach($lists as $login){
+	$log = "$targetnya/$login";
+	$ch = curl_init("$log");
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_exec($ch);
+	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	if($httpcode == 200){
+		 $handle = fopen("result.txt", "a+");
+		fwrite($handle, "$log\n");
+		print "\n\n [".date('H:m:s')."] Mencoba : $log => Ditemukan\n";
+	}else{
+		print "\n[".date('H:m:s')."] Mencoba : $log => tidak di temukan";
+	}
+}
+  
+?>
